fdeefaa28db7df2435f1cdc7029486c47795e656
mframos3 <mframos3@uc.cl>
2019-09-29 23:57:14 -0300

    :WIP: minor fixes.

--- CMWeb/Data/Migrations/20190930024832_Scaffold.Designer.cs
+++ CMWeb/Data/Migrations/20190930024832_Scaffold.Designer.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using CMWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CMWeb.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190930024832_Scaffold")]
+    partial class Scaffold
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
