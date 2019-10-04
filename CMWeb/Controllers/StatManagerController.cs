f6d1c98ce6c049f2df85466dd5e663e7bc9642ee
Erick R <egromero@uc.cl>
2019-10-03 23:35:33 -0300

    :rotating_light: migrations problems, button to stats, fix some links

--- CMWeb/Controllers/StatManagerController.cs
+++ CMWeb/Controllers/StatManagerController.cs
@@ -1,3 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CMWeb.Data;
+using CMWeb.Models;
+
+
+namespace CMWeb.Controllers
+{
+    public class StatManagerController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 /*
 using System;
 using System.Collections.Generic;
