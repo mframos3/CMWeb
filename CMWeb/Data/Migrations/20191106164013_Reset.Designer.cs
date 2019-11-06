13eb72ebca33c99f614c1e5c5e2909c226918419
PhilipBarr <pbarr@uc.cl>
2019-11-06 13:50:14 -0300

    migration hotfix

--- CMWeb/Data/Migrations/20191106164013_Reset.Designer.cs
+++ CMWeb/Data/Migrations/20191106164013_Reset.Designer.cs
@@ -2,17 +2,20 @@
 using System;
 using System.Collections.Generic;
 using CMWeb.Data;
+using CMWeb.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CMWeb.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191106164013_Reset")]
+    partial class Reset
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
