0f02169d16057708d4d0cc1de9ed49ff1f2291b0
mframos3 <mframos3@uc.cl>
2019-12-20 12:52:55 -0300

    :ambulance: Conference DateChecker added.

--- CMWeb/Models/Conference.cs
+++ CMWeb/Models/Conference.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CMWeb.Models
 {
@@ -11,8 +12,12 @@ namespace CMWeb.Models
         public string Edition { get; set; }
         public string Description { get; set; }
 
-        [DataType(DataType.Date)] public DateTime StartDate { get; set; }
-        [DataType(DataType.Date)] public DateTime EndDate { get; set; }
+        [DataType(DataType.DateTime)] 
+        [Remote(action: "DateChecker", controller: "Conference", AdditionalFields = "EndDate")]
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.DateTime)] 
+        [Remote(action: "DateChecker", controller: "Conference", AdditionalFields = "StartDate")]
+        public DateTime EndDate { get; set; }
 
         public ICollection<Event> Events { get; set; }
 
